a8ea5eda4020eb78c146bd357db699cac9af3f06
Piotr Karpala <piotrkarpala@microsoft.com>
2025-01-03 15:27:52 -0500

    fixes and reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,7 +22,16 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: process.env.CI ? 
+  [
+    ['html', { open: 'never' }],
+    ['github'],
+    ['junit', { outputFile: 'results.xml' }]
+  ] : 
+  [
+    ['list'],
+    ['html', { open: 'never' }]
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
