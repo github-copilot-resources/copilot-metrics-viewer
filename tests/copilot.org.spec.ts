0d27a2ae2903575100130bd0ab16f580bf588051
Piotr Karpala <piotrkarpala@microsoft.com>
2025-01-03 14:39:31 -0500

    Playwright tests

---
+++ tests/copilot.org.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+
+const tag = { tag: ['@org'] }
+
+test('has title', tag, async ({ page }) => {
+  await page.goto('/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/copilot-metrics-viewer/);
+  await expect(page.locator(".toolbar-title")).toHaveText(/Copilot Metrics Viewer \| Organization : octodemo/);
+});
+
+test('metrics visible', tag, async ({ page }) => {
+  await page.goto('/');
+
+  await expect(page.getByText('Acceptance Rate Average')).toBeVisible();
+  await expect(page.getByText('Cumulative Number of Suggestions')).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Total Lines Suggested | Total' })).toBeVisible();
+  await page.getByRole('tab', { name: 'languages' }).click();
+  await expect(page.getByText('Top 5 Languages by acceptance')).toBeVisible();
+  await page.getByRole('tab', { name: 'editors' }).click();
+  await expect(page.getByText('Number of Editors')).toBeVisible();
+  await page.getByRole('tab', { name: 'seat analysis' }).click();
+  await expect(page.getByText('Total Assigned')).toBeVisible();
+});
+
+test('organization tab', tag, async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByRole('tab', { name: 'organization' })).toBeVisible();
+});
