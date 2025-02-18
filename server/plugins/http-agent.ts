71bc8ecf7c1c7a9f305b5aa01403a6808f14b232
Huynh Duc Tran <110240973+hdt12a1@users.noreply.github.com>
2025-02-18 08:54:31 +0700

    CUSTOM_CA_PATH option for HTTPS PROXY - feature/https-proxy (#162)
    * .
    
    * Update Readme and version 2.0.2
    
    ---------
    
    Co-authored-by: Piotr Karpala <karpik.pl@gmail.com>

--- server/plugins/http-agent.ts
+++ server/plugins/http-agent.ts
@@ -1,12 +1,31 @@
-import { ProxyAgent, setGlobalDispatcher } from "undici";
-import { ofetch } from "ofetch";
+import { ProxyAgent, setGlobalDispatcher } from 'undici';
+import { ofetch } from 'ofetch';
+import { readFileSync } from 'fs';
 
 export default defineNitroPlugin((nitro) => {
   if (process.env.HTTP_PROXY) {
-    const proxyAgent = new ProxyAgent(process.env.HTTP_PROXY);
+    const tlsOptions = process.env.CUSTOM_CA_PATH ? {
+      tls: {
+        ca: [
+          readFileSync(process.env.CUSTOM_CA_PATH)
+        ]
+      }
+    } : {};
+
+    const proxyAgent = new ProxyAgent({
+      uri: process.env.HTTP_PROXY,
+      ...tlsOptions
+    });
+    
     setGlobalDispatcher(proxyAgent);
-    const fetchWithProxy = ofetch.create({ dispatcher: proxyAgent });
-    nitro.hooks.hook("request", (context) => {
+    
+    const fetchWithProxy = ofetch.create({
+      dispatcher: proxyAgent,
+      httpsProxy: process.env.HTTP_PROXY,
+      proxy: false
+    });
+
+    nitro.hooks.hook('request', (context) => {
       context.fetch = fetchWithProxy;
     });
   }
