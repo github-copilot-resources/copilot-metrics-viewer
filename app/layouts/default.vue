38bacb68b8b444a62546bc7ee235d286f19d2cbc
Elliot Moore <elliot.moore@bazaarvoice.com>
2025-02-25 18:25:03 +0000

    Update default.vue
    Minor Edit
    Add your Github project URL as a link 
    I find it useful to refer to your github repo for this project!

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -5,7 +5,7 @@
     </v-main>
     <v-footer class="bg-indigo-lighten-1 text-center d-flex flex-column fixed-footer">
       <div class="px-4 py-2 text-center w-100">
-        {{ new Date().getFullYear() }} — <strong>Copilot Metrics Viewer</strong> — {{ version }}
+        {{ new Date().getFullYear() }} — <strong><a href="https://github.com/github-copilot-resources/copilot-metrics-viewer" target="_blank" rel="noopener noreferrer" style="color: inherit;">Copilot Metrics Viewer</a></strong> — {{ version }}
       </div>
     </v-footer>
   </v-app>
